814a4237d50937fb3aea22af277ec495e006ff5f
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-03-25 11:36:06 -0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    parser: 'babel-eslint',
+    env: {
+      browser: true,
+      node: true,
+      jest: true
+    },
+    extends: 'airbnb',
+    rules: {
+      'no-tabs': 0,
+      'react/jsx-one-expression-per-line': false,
+      'react/jsx-indent': ['error', 2],
+      'no-console': 'off'
+    }
+  };
