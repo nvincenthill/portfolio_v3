616c62897f1aad84a7c3b1d0e6d79bd81eaa1a1d
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-03-28 15:25:08 -0700

    Add articles page

---
+++ src/pages/articles.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+import Layout from '../components/layout';
+import Article from '../components/Article';
+import articles from '../assets/data/blog-posts.json';
+
+class ArticlesPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    const siteTitle = 'Articles';
+    const siteDescription =
+      "A collection of Nicholas Vincent-Hill's written work";
+
+    return (
+      <Layout>
+        <Helmet>
+          <title>{siteTitle}</title>
+          <meta name="description" content={siteDescription} />
+        </Helmet>
+
+        <div id="main">
+          <section id="one">
+            <header className="major">
+              <h2>Here are some things I've written</h2>
+            </header>
+            {articles.map(article => {
+              return <Article key={article.title} article={article} />;
+            })}
+          </section>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default ArticlesPage;
