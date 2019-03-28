8d07b0c7a19a6294c3e69400debbffc390c9809c
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-03-28 15:26:33 -0700

    Add layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,21 @@
-import React from "react"
-import "../assets/scss/main.scss"
+import React from 'react';
+import '../assets/scss/main.scss';
 
-import Header from "./Header"
+import Header from './Header';
 
 class Template extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    const { children } = this.props
-
+    const { children } = this.props;
     return (
       <div>
         <Header />
         {children}
       </div>
-    )
+    );
   }
 }
 
-export default Template
+export default Template;
