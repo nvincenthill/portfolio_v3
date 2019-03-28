4e31d18b70cb1ccaa7cdcb59f47ec01452da2c2f
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-03-28 15:25:46 -0700

    Add NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      typeof window !== 'undefined' && (
+        <div>
+          <h1 className={window.location.pathname === '/about/' ? 'bold' : ''}>
+            <Link to="/about/">About</Link>
+          </h1>
+          <h1
+            className={window.location.pathname === '/portfolio/' ? 'bold' : ''}
+          >
+            <Link to="/portfolio/">Portfolio</Link>
+          </h1>
+          <h1
+            className={window.location.pathname === '/articles/' ? 'bold' : ''}
+          >
+            <Link to="/articles/">Articles</Link>
+          </h1>
+          <h1
+            className={window.location.pathname === '/contact/' ? 'bold' : ''}
+          >
+            <Link to="/contact/">Contact me</Link>
+          </h1>
+        </div>
+      )
+    );
+  }
+}
+
+export default NavBar;
