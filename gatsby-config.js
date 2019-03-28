9e503c514d8a2e554babbf64c85bed9bd1c9a4ea
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-03-28 15:22:58 -0700

    Add dependencies and s3 deploy config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,45 @@
+const path = require('path');
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
-    title: "Nicholas Vincent-Hill",
-    description: "A simple portfolio site built with Gatsby",
-    author: "Nicholas Vincent-Hill",
+    title: 'Nicholas Vincent-Hill',
+    description: 'A simple portfolio site built with Gatsby',
+    author: 'Nicholas Vincent-Hill',
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
-        start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
-        display: "minimal-ui",
-        icon: "src/assets/images/avatar.jpg",
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/avatar.jpg',
       },
     },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-offline",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: path.join(__dirname, 'src', 'assets', 'images'),
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-s3',
+      options: {
+        bucketName: 'nickvh.tech',
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
   ],
-}
+};
