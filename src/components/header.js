6df32ea60bb93898371ef6b6ab95559010824f54
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-03-28 15:25:36 -0700

    Add header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,50 @@
-import React from "react"
+import React from 'react';
 
-import Footer from "./Footer"
-import avatar from "../assets/images/avatar.jpg"
+import Footer from './Footer';
+import NavBar from './NavBar';
+import avatar from '../assets/images/avatar.jpg';
+import Particles from 'react-particles-js';
+import particleConfig from '../assets/data/particlesjs-config.json';
+import particleConfigSpace from '../assets/data/particlesjs-config-space.json';
+
+import { Link } from 'gatsby';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <header id="header">
         <div className="inner">
-          <a href="#" className="image avatar">
-            <img src={avatar} alt="" />
-          </a>
+          <div className="outer-particle-container">
+            <div className="inner-particle-container">
+              <Link to="/">
+                <img className="image avatar" src={avatar} alt="" />
+              </Link>
+
+              {typeof window !== 'undefined' && (
+                <Particles
+                  height={200}
+                  params={
+                    window.location.pathname === '/contact/'
+                      ? particleConfig
+                      : particleConfigSpace
+                  }
+                />
+              )}
+            </div>
+          </div>
           <h1>
-            I'm an engineer
-            <br />
-            designing responsive
-            <br />
-            mobile-first web applications
+            <strong>Nicholas Vincent-Hill</strong>
           </h1>
+          <NavBar />
         </div>
         <Footer />
       </header>
-    )
+    );
   }
 }
 
-export default Header
+export default Header;
