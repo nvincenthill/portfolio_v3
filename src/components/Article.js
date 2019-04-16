90eb6b72887bdf151c3398ca4d925a0d1f725eed
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-04-16 10:07:41 -0700

    Refactor to stateless component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
-class Article extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { article } = this.props;
-    return (
-      <div>
-        <a href={article.url} target="_blank">
-          <h2>{article.title}</h2>
-        </a>
-        <p>{article.description}</p>
-      </div>
-    );
-  }
-}
+import PropTypes from 'prop-types';
+
+const Article = (props) => {
+  const { article } = props;
+  return (
+    <div>
+      <a href={article.url} target="_blank" rel="noopener noreferrer">
+        <h2>{article.title}</h2>
+      </a>
+      <p>{article.description}</p>
+    </div>
+  );
+};
+
+Article.propTypes = {
+  article: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Article;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Image from './Image';
 
-class Project extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { project } = this.props;
-    return (
-      <a href={project.github} className="project-container" target="_blank">
-        <div className="project-image-container">
-          <Image src={project.imageSource} />
-        </div>
-        <h2 className="project-title">{project.name}</h2>
-        <h3 className="project-description">{project.description}</h3>
-      </a>
-    );
-  }
-}
+const Project = (props) => {
+  const { project } = props;
+  return (
+    <a
+      href={project.github}
+      className="project-container"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="project-image-container">
+        <Image src={project.imageSource} />
+      </div>
+      <h2 className="project-title">{project.name}</h2>
+      <h3 className="project-description">{project.description}</h3>
+    </a>
+  );
+};
+
+Project.propTypes = {
+  project: PropTypes.shape({
+    imageSource: PropTypes.string.isRequired,
+    github: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Project;
