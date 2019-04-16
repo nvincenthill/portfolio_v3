249fd125b964611fc377c28e4475ec2afe4b1cf3
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-04-16 10:11:09 -0700

    Refactor to stateless component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,39 +1,37 @@
 import React from 'react';
 import Img from 'gatsby-image';
 import { StaticQuery, graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 
 function renderImage(file) {
   return <Img fluid={file.node.childImageSharp.fluid} />;
 }
 
-const MyImg = function(props) {
-  return (
-    <StaticQuery
-      query={graphql`
-        query {
-          images: allFile(
-            filter: { extension: { regex: "/jpeg|jpg|png|gif/" } }
-          ) {
-            edges {
-              node {
-                extension
-                relativePath
-                childImageSharp {
-                  fluid(maxWidth: 1000) {
-                    ...GatsbyImageSharpFluid
-                  }
+const Image = props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        images: allFile(filter: { extension: { regex: "/jpeg|jpg|png|gif/" } }) {
+          edges {
+            node {
+              extension
+              relativePath
+              childImageSharp {
+                fluid(maxWidth: 1000) {
+                  ...GatsbyImageSharpFluid
                 }
               }
             }
           }
         }
-      `}
-      render={({ images }) =>
-        renderImage(
-          images.edges.find(image => image.node.relativePath === props.src)
-        )
       }
-    />
-  );
+    `}
+    render={({ images }) => renderImage(images.edges.find(image => image.node.relativePath === props.src))}
+  />
+);
+
+Image.propTypes = {
+  src: PropTypes.string.isRequired,
 };
-export default MyImg;
+
+export default Image;
