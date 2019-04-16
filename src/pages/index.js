56dbd9684752780d6e9673ec783fed37485d12fd
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-04-16 10:08:54 -0700

    Add simulation to splash page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 import Layout from '../components/layout';
+import PhysicsEngine from '../components/PhysicsEngine';
 
 class Home extends React.Component {
   constructor() {
     super();
-
     this.state = {};
   }
 
@@ -24,37 +24,13 @@ class Home extends React.Component {
         <div id="main">
           <section id="one">
             <header className="major">
-              <h2>Hi, I'm Nick!</h2>
-              <h3>
-                I'm a software engineering instructor @ Hack Reactor at
-                Galvanize.
-              </h3>
+              <h1>Hi, I&#39;m Nick Vincent-Hill</h1>
+              <h3>I&#39;m a software engineering instructor @ Hack Reactor at Galvanize</h3>
             </header>
-            {/* <div className="poem">
-              <p>I met a traveller from an antique land,</p>
-              <p>Who said—“Two vast and trunkless legs of stone</p>
-              <p>Stand in the desert. . . . Near them, on the sand,</p>
-              <p>Half sunk a shattered visage lies, whose frown,</p>
-              <p>And wrinkled lip, and sneer of cold command,</p>
-              <p>Tell that its sculptor well those passions read</p>
-              <p>Which yet survive, stamped on these lifeless things,</p>
-              <p>The hand that mocked them, and the heart that fed;</p>
-              <p>And on the pedestal, these words appear:</p>
-              <p className="poem-hightlight">
-                My name is Ozymandias, King of Kings;
-              </p>
-              <p className="poem-hightlight">
-                Look on my Works, ye Mighty, and despair!
-              </p>
-              <p>Nothing beside remains. Round the decay</p>
-              <p>Of that colossal Wreck, boundless and bare</p>
-              <p>The lone and level sands stretch far away.</p>
-              <br />
-              <p>
-                <strong>Percy B. Shelley</strong>
-              </p>
-            </div> */}
           </section>
+        </div>
+        <div id="splash-graphic">
+          <PhysicsEngine />
         </div>
       </Layout>
     );
