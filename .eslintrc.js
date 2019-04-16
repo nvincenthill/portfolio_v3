24800653fd919bdb9025d2f006827ac54b447e21
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-04-15 21:09:50 -0700

    Ignore airbnb style rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    parser: 'babel-eslint',
-    env: {
-      browser: true,
-      node: true,
-      jest: true
-    },
-    extends: 'airbnb',
-    rules: {
-      'no-tabs': 0,
-      'react/jsx-one-expression-per-line': false,
-      'react/jsx-indent': ['error', 2],
-      'no-console': 'off'
-    }
-  };
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  extends: 'airbnb',
+  rules: {
+    'no-tabs': 0,
+    'react/jsx-one-expression-per-line': false,
+    'react/jsx-indent': ['error', 2],
+    'no-console': 'off',
+    'react/jsx-filename-extension': false,
+  },
+};
